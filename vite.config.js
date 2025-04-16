cba5cc61729747ac14f922bfb3b00cef4e6471e8
adnim811 <adnim@umich.edu>
2025-04-16 19:18:11 -0400

    Update favicon and script paths to use relative paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -16,17 +16,17 @@ export default defineConfig({
       input: resolve(__dirname, 'index.html'),
       output: {
         assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `assets/images/[name]-[hash][extname]`
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images';
+          } else if (extType === 'css') {
+            extType = 'css';
           }
-          return `assets/[name]-[hash][extname]`
+          return `assets/${extType}/[name]-[hash][extname]`;
         },
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
-        manualChunks: undefined
-      }
+      },
     },
     sourcemap: true,
     manifest: true,
